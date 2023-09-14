3f2bb821c9fc51c46579e5a23cdc217385b2d49f
Adrian <adunham95@gmail.com>
2023-09-14 15:45:45 -0400

    Added favicons

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,17 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+
+
+<link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png"/>
+<link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png"/>
+<link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png"/>
+<link rel="manifest" href="/favicon/site.webmanifest"/>
+<link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#62b7fb"/>
+<meta name="msapplication-TileColor" content="#62b7fb"/>
+<meta name="theme-color" content="#62b7fb"/>
+        </Head>
       <body>
         <Main />
         <NextScript />
--- pages/index.tsx
+++ pages/index.tsx
@@ -10,10 +10,15 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
+    <>
+    <Head>
+    <title>DishLab</title>
+    </Head>
     <DefaultLayout>
       <Button variant='contained' color='secondary'>Button</Button>
       <Logo/>
       <LogoSquare/>
     </DefaultLayout>
+    </>
   )
 }
